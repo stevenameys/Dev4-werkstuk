75b625c9c94aa1fe4900299d8bf9565d75c0131e
stevenameys <steven.ameys@student.ehb.be>
2021-08-12 18:11:38 +0200

    feat(algorithm): added a list with currency and the quantity of that currency to the algorithm.js file

---
+++ algorithm.js
@@ -0,0 +1,34 @@
+let coinList = [
+    {
+        coin: 200,
+        quantity: 5
+    }, 
+    {
+        coin: 100,
+        quantity: 0
+    }, 
+    {
+        coin: 50,
+        quantity: 1
+    }, 
+    {
+        coin: 20,
+        quantity: 4
+    }, 
+    {
+        coin: 10,
+        quantity: 1
+    }, 
+    {
+        coin: 5,
+        quantity: 3
+    }, 
+    {
+        coin: 2,
+        quantity: 4
+    }, 
+    {
+        coin: 1,
+        quantity: 20
+    }
+];
