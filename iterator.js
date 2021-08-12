e4dfc6d9023525df5f9c2d1b689dbeaa47dbc679
stevenameys <steven.ameys@student.ehb.be>
2021-08-12 18:03:02 +0200

    feat(iterator): added example of how the iterator would behave if the slotNr does not exist

--- iterator.js
+++ iterator.js
@@ -114,3 +114,20 @@ if (searchSlot == null){
 }else{
   console.log(`You have picked ${searchSlot.name} for €${searchSlot.price}`);
 }
+
+const SlotIt2 = Iterator(0, 12, 1, slotList);
+result = SlotIt2.next();
+let searchSlot2;
+selectedSlot = "C4";
+
+while (!result.done) {
+  if (result.slotNr == selectedSlot){
+    searchSlot2 = result;
+  }
+  result = SlotIt2.next();
+}
+if (searchSlot2 == null){
+  console.log("Slot does not exist")
+}else{
+  console.log(`You have picked ${searchSlot2.name} for €${searchSlot2.price}`);
+}
