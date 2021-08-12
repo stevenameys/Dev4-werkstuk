2d560dcc9a9399e6c608b3b26abb191571560f0e
stevenameys <steven.ameys@student.ehb.be>
2021-08-12 17:57:07 +0200

    feat(iterator): added a list of soda and snacks for the iterator to run through

---
+++ iterator.js
@@ -0,0 +1,70 @@
+let slotList = [
+    {
+      slotNr: "A1",
+      name: "Coca Cola",
+      price: 2.50,
+      quantity: 10
+    },
+    {
+      slotNr: "A2",
+      name: "Fanta",
+      price: 2.20,
+      quantity: 5
+    },
+    { slotNr: "A3",
+      name: "Sprite",
+      price: 2.25,
+      quantity: 0
+    },
+    {
+      slotNr: "B1",
+      name: "Chaudfontaine",
+      price: 1.75,
+      quantity: 6
+    },
+    { slotNr: "B2",
+      name: "Nalu",
+      price: 2.30,
+      quantity: 1
+    },
+    {
+      slotNr: "B3",
+      name: "Fuze Tea",
+      price: 2.35,
+      quantity: 8
+    },
+    {
+      slotNr: "C1",
+      name: "Bounty",
+      price: 2.20,
+      quantity: 9
+    },
+    {
+      slotNr: "C2",
+      name: "Mentos",
+      price: 1.85,
+      quantity: 4
+    },
+    { slotNr: "C3",
+      name: "Skittles",
+      price: 2.95,
+      quantity: 6
+    },
+    {
+      slotNr: "D1",
+      name: "Twix",
+      price: 1.95,
+      quantity: 3
+    },
+    { slotNr: "D2",
+      name: "Snickers",
+      price: 1.50,
+      quantity: 6
+    },
+    {
+      slotNr: "D3",
+      name: "Mars",
+      price: 2.00,
+      quantity: 6
+    }
+];
