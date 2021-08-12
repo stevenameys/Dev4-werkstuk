c05935ea3953329eccfa4847e433c0c270736324
stevenameys <steven.ameys@student.ehb.be>
2021-08-12 18:14:13 +0200

    feat(algorithm): added logic for the algorithm to sort the change by using the current value

--- algorithm.js
+++ algorithm.js
@@ -32,3 +32,31 @@ let coinList = [
         quantity: 20
     }
 ];
+
+function returnCoins(coins, price, paid){
+    let returnCoinList = [];
+    let toReturn = paid - price;
+    let i = 0;
+
+    while (toReturn > 0){
+        while(toReturn < coins[i].coin){ 
+            i++
+        }
+        while(coins[i].quantity == 0){
+            if(i + 1 > coins.length - 1){
+                returnCoinList.push("can not return all money");
+                return returnCoinList;
+            }
+            i++
+        }
+
+        toReturn -= coins[i].coin;
+        coins[i].quantity--;
+        returnCoinList.push(coins[i].coin);
+        i = 0;
+    }
+    return returnCoinList;
+}
+
+let getChange = returnCoins(coinList, 132, 500);
+console.log(getChange);
